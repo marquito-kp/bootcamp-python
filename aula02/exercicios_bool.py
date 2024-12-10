89f69297ebf2f0b6f17dae434f6f23a21654ef1a
Marcos Krol Pacheco <marcos.krolp@gmail.com>
2024-12-09 23:07:49 -0300

    Desenvolvimento da aula 02 do Bootcamp

---
+++ aula02/exercicios_bool.py
@@ -0,0 +1,16 @@
+# Exercicios aula 2
+
+# Booleanos (bool)
+
+# 1) Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação 
+# AND entre elas.
+
+
+
+# 2) Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+
+# 3) Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+
+# 4) Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+
+# 5) Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
